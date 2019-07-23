037ee4485007405e6faad4d8182c9d511355e276
sicon2002@163.com <sicon20022163.com>
2019-07-23 22:15:04 +0800

    修改了index的title

--- static/js/my.js
+++ static/js/my.js
@@ -1,5 +1,5 @@
 var sys = {};
 
-sys.hostUrl = "http://www.sicon.xyz:5000";
+//sys.hostUrl = "http://www.sicon.xyz:5000";
 
-//sys.hostUrl = "http://localhost:5000";
+sys.hostUrl = "http://localhost:5000";
